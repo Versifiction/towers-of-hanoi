9ebf3a4ceba093c24588e3ec81aa4aca14940ea8
Marc <marc_charpentier@hotmail.fr>
2022-12-31 15:36:11 +0100

    first version of the game

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,110 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import classnames from "classnames";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const DISCS_NUMBER = 5;
+
+const App = () => {
+  const [towers, setTowers] = useState();
+  const [firstTowerSelected, setFirstTowerSelected] = useState(-1);
+  const [secondTowerSelected, setSecondTowerSelected] = useState(-1);
+  const bothTowersSelected =
+    firstTowerSelected !== -1 && secondTowerSelected !== -1;
+
+  const handleClick = (index) => {
+    if (firstTowerSelected === -1 && !bothTowersSelected) {
+      setFirstTowerSelected(index);
+    } else {
+      setSecondTowerSelected(index);
+    }
+  };
+
+  const generateTowers = () => {
+    let towers = [];
+
+    for (let i = 0; i < DISCS_NUMBER - 1; i++) {
+      towers.push([]);
+    }
+
+    let j = 0;
+
+    while (j < DISCS_NUMBER) {
+      j++;
+      setTowers([[...new Array(DISCS_NUMBER)].map(() => j++), ...towers]);
+    }
+  };
+
+  useEffect(() => {
+    console.log(towers);
+  }, [towers]);
+
+  useEffect(() => {
+    generateTowers();
+  }, []);
+
+  useEffect(() => {
+    if (
+      bothTowersSelected &&
+      towers[firstTowerSelected][0] <
+        (towers[secondTowerSelected][0] || DISCS_NUMBER + 1)
+    ) {
+      towers[secondTowerSelected].unshift(towers[firstTowerSelected][0]);
+      towers[firstTowerSelected].shift();
+
+      setFirstTowerSelected(-1);
+      setSecondTowerSelected(-1);
+
+      if (towers[towers.length - 1].length === DISCS_NUMBER) {
+        alert("GG");
+      }
+    }
+
+    if (
+      bothTowersSelected &&
+      !towers[firstTowerSelected][0] <
+        (towers[secondTowerSelected][0] || DISCS_NUMBER + 1)
+    ) {
+      setFirstTowerSelected(-1);
+      setSecondTowerSelected(-1);
+    }
+  }, [firstTowerSelected, secondTowerSelected]);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="App flex justify-between items-center bg-[#808080] w-full h-full relative">
+      {towers?.map((tower, index) => (
+        <>
+          <div className="flex flex-col cursor-pointer relative" key={index}>
+            <span
+              className={classnames(
+                {
+                  "bg-white":
+                    firstTowerSelected === index && !bothTowersSelected,
+                  "bg-green-900":
+                    firstTowerSelected !== index || bothTowersSelected,
+                },
+                "w-5 absolute -translate-x-2/4 left-2/4 -top-2.5 -bottom-2.5 z-10"
+              )}
+              onClick={() => handleClick(index)}
+            ></span>
+            {tower?.map((t, idx) => (
+              <span
+                className="px-6 py-1 bg-black text-white border-2 z-20"
+                key={idx}
+              >
+                {t}
+              </span>
+            ))}
+          </div>{" "}
+        </>
+      ))}
+      <button
+        className="px-6 py-1 absolute -translate-x-2/4 left-2/4 bottom-1 bg-black text-white border-2"
+        onClick={generateTowers}
+      >
+        Rejouer
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
